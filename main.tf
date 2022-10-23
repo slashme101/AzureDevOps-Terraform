905d07e31fd9a91a4af503e016730303cda8b568
Rodney <rodney.effah@outlook.com>
2022-10-23 21:59:16 +0100

    updated main

--- main.tf
+++ main.tf
@@ -13,6 +13,11 @@ terraform {
   }
 }
 
+variable "imagebuild" {
+  type = string
+  description = "Latest Image Build"
+  
+}
 resource "azurerm_resource_group" "tf_test" {
     name = "tfmainrg"
     location =   "east us"
@@ -29,7 +34,7 @@ resource "azurerm_container_group" "tfcg_test" {
 
     container {
         name = "weatherapi"
-        image = "slashme101/weatherapi"
+        image = "slashme101/weatherapi:${var.imagebuild}"
           cpu = "1"
           memory = "1"
 
